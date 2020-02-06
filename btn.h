bfc9b10b5deb31638b1958dfac4aabab60e4f888
Sandro Magi <naasking@gmail.com>
2020-02-05 22:18:40 -0500

    rename for consistency

--- btn.h
+++ btn.h
@@ -11,7 +11,7 @@
 typedef struct {
   unsigned char active : 1;
   unsigned char count : 8 * sizeof(unsigned char) - 1;
-} btn_poll;
+} btn_sync;
 
 /**
  * Poll the button's status.
@@ -19,7 +19,7 @@ typedef struct {
  * Returns: true if button value has settled, false if it's bouncing.
  * btn->active always reflects the last settled button state.
  */
-static unsigned btn_poll(unsigned pin, btn_poll* btn, unsigned limit) {
+static unsigned btn_poll(unsigned pin, btn_sync* btn, unsigned limit) {
   unsigned active = digitalRead(pin);
   /* button state changes after the current status is seem 'limit' times */
   if (active != btn->active && limit < ++btn->count) {
@@ -38,8 +38,8 @@ static unsigned btn_poll(unsigned pin, btn_poll* btn, unsigned limit) {
  * Button data structure tracking currently active state and a counter for debouncing.
  */
 typedef struct {
-  unsigned active : 1;
-  unsigned long last : 8 * sizeof(unsigned)	 - 1;
+  unsigned state : 1;
+  unsigned long last : 8 * sizeof(unsigned)   - 1;
 } btn_async;
 
 /**
@@ -52,7 +52,7 @@ static void btn_onchange(btn_async* btn) {
 }
 
 /**
- * Checkthe button's status.
+ * Check the button's status.
  * 
  * Returns: true if button value has settled, false if it's bouncing.
  * btn->active always reflects the last settled button state.
@@ -60,7 +60,7 @@ static void btn_onchange(btn_async* btn) {
 static unsigned btn_ready(unsigned pin, btn_async* btn, unsigned delay) {
   /* button state is updated after 'delay' has elapsed */
   if (millis() - btn->last > delay) {
-    btn->active = digitalRead(pin);
+    btn->state = digitalRead(pin);
     btn->last = 0;
     return 1;
   }
